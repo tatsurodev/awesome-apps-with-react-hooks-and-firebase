59f5bb6fd5a6c7f4df2a78c83e616d7ae0471bfa
Tatsuro <contacts@tatsuro.dev>
2020-10-29 17:36:00 +0900

    custom hookでformの入力値をstateに保存 8

--- hooks-news/src/components/Auth/Login.js
+++ hooks-news/src/components/Auth/Login.js
@@ -1,18 +1,48 @@
 import React from 'react'
+import useFormValidation from './useFormValidation'
+
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  password: '',
+}
 
 function Login(props) {
+  const { handleChange, handleSubmit, values } = useFormValidation(
+    INITIAL_STATE
+  )
   const [login, setLogin] = React.useState(true)
 
   return (
     <div>
       <h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
-      <form className="flex flex-column">
+      <form onSubmit={handleSubmit} className="flex flex-column">
         {/* loginがfalseの時右、つまりregisterの時右 */}
         {login || (
-          <input type="text" placeholder="Your name" autoComplete="off" />
+          <input
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+          />
         )}
-        <input type="email" placeholder="Your email" autoComplete="off" />
-        <input type="password" placeholder="Choose a secure password" />
+        <input
+          onChange={handleChange}
+          value={values.email}
+          name="email"
+          type="email"
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        <input
+          onChange={handleChange}
+          value={values.password}
+          name="password"
+          type="password"
+          placeholder="Choose a secure password"
+        />
         <div className="flex mt3">
           <button type="submit" className="button pointer mr2">
             Submit
