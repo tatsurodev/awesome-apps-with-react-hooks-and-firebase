4c08977cde9a5dc491949b0e32b3f1338b7a814c
Tatsuro <contacts@tatsuro.dev>
2020-10-29 18:21:13 +0900

    validation hook作成 9

--- hooks-news/src/components/Auth/validateLogin.js
+++ hooks-news/src/components/Auth/validateLogin.js
@@ -1,1 +1,19 @@
-export default function validateLogin(values) {}
+export default function validateLogin(values) {
+  let errors = {}
+
+  // email errors
+  if (!values.email) {
+    errors.email = 'Email requried'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  // passowrd errors
+  if (!values.password) {
+    errors.password = 'Password required'
+  } else if (values.password.length < 6) {
+    errors.password = 'Passowrd must be at least 6 characters'
+  }
+
+  return errors
+}
