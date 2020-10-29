22c419aec20347c8cb887012af68d9d971027dab
Tatsuro <contacts@tatsuro.dev>
2020-10-29 10:40:41 +0900

    Header作成、withRouterでhistoryにaccessできるようにする 6

--- hooks-news/src/components/Header.js
+++ hooks-news/src/components/Header.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { withRouter, NavLink } from 'react-router-dom'
 
-function Header() {
-  return <div>Header</div>
+function Header(props) {
+  // withRouterでrouting対象でないcomponentでprops.historyを使用できるようにする。hocなので引数のcomponentをwrapする
+  // console.log(props.history)
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <div className="divider">|</div>
+        <NavLink to="/login" className="header-link">
+          login
+        </NavLink>
+      </div>
+    </div>
+  )
 }
 
-export default Header
+export default withRouter(Header)
