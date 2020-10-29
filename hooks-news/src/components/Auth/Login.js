8dc88a2318a9e3b0802d36b7890a16e7079c342e
Tatsuro <contacts@tatsuro.dev>
2020-10-29 11:37:57 +0900

    loginとsigninをtoggle 7

--- hooks-news/src/components/Auth/Login.js
+++ hooks-news/src/components/Auth/Login.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
 function Login(props) {
-  return <div>Login</div>
+  const [login, setLogin] = React.useState(true)
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
+      <form className="flex flex-column">
+        {/* loginがfalseの時右、つまりregisterの時右 */}
+        {login || (
+          <input type="text" placeholder="Your name" autoComplete="off" />
+        )}
+        <input type="email" placeholder="Your email" autoComplete="off" />
+        <input type="password" placeholder="Choose a secure password" />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+            // onClick={() => setLogin(!login)}
+          >
+            {login ? 'need to create an account?' : 'already have an account?'}
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login
