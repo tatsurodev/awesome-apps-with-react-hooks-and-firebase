ac436702ff998c67d9e70757339bf73968973861
Tatsuro <contacts@tatsuro.dev>
2020-10-29 22:09:54 +0900

    firebaseからlogin userを取得するuseAuth hookを作成 13

--- hooks-news/src/components/Auth/useAuth.js
+++ hooks-news/src/components/Auth/useAuth.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import firebase from '../../firebase'
 
-function useAuth() {}
+function useAuth() {
+  const [authUser, setAuthUser] = useState(null)
+
+  useEffect(() => {
+    // listenerをreturn
+    const unsubscribe = firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthUser(user)
+      } else {
+        setAuthUser(null)
+      }
+    })
+    // cleaning up listener
+    return () => unsubscribe()
+  }, [])
+
+  return authUser
+}
 
 export default useAuth
