a9537aa4045eac67f8f8205c075317a54912de3b
Tatsuro <contacts@tatsuro.dev>
2020-10-29 21:18:42 +0900

    firebase setup 10

---
+++ hooks-news/src/firebase/firebase.js
@@ -0,0 +1,30 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './config'
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig)
+    this.auth = app.auth()
+  }
+
+  async register(name, email, password) {
+    // user作成
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    )
+    console.log(newUser)
+    // 作成したuserにnameをset
+    return await newUser.user.updateProfile({
+      displayName: name,
+    })
+  }
+
+  async login(email, password) {
+    return await this.auth.signInWithEmailAndPassword(email, password)
+  }
+}
+
+const firebase = new Firebase()
+export default firebase
