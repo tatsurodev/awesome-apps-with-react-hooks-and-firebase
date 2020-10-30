d715a561f4cb5c3ef2f709ec7d693ab3af6be172
Tatsuro <contacts@tatsuro.dev>
2020-10-31 05:26:37 +0900

    firestoreへlinkを保存 18

--- hooks-news/src/components/Link/CreateLink.js
+++ hooks-news/src/components/Link/CreateLink.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import useFormValidation from '../Auth/useFormValidation'
 import validateCreateLink from '../Auth/validateCreateLink'
+import { FirebaseContext } from '../../firebase'
 
 const INITIAL_STATE = {
   description: '',
   url: '',
 }
 function CreateLink(props) {
+  const { firebase, user } = React.useContext(FirebaseContext)
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
@@ -14,8 +16,26 @@ function CreateLink(props) {
   )
 
   function handleCreateLink() {
-    console.log('link created!')
+    if (!user) {
+      props.history.push('/login')
+    } else {
+      const { url, description } = values
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName,
+        },
+        votes: [],
+        comments: [],
+        created: Date.now(),
+      }
+      firebase.db.collection('links').add(newLink)
+      props.history.push('/')
+    }
   }
+
   return (
     <form onSubmit={handleSubmit} className="flex flex-column mt3">
       <input
@@ -34,7 +54,7 @@ function CreateLink(props) {
         name="url"
         placeholder="The URL for the link"
         autoComplete="off"
-        type="text"
+        type="url"
         className={errors.url && 'error-input'}
       />
       {errors.url && <p className="error-text">{errors.url}</p>}
