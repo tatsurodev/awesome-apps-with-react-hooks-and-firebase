1a5cfd82d5d4198c83eb03e7c41c6d749d4d85d6
Tatsuro <contacts@tatsuro.dev>
2020-11-01 21:15:46 +0900

    pagination実装 27

--- hooks-news/src/components/Link/LinkList.js
+++ hooks-news/src/components/Link/LinkList.js
@@ -1,30 +1,48 @@
 import React, { useState } from 'react'
 import FirebaseContext from '../../firebase/context'
 import LinkItem from './LinkItem'
+import { LINKS_PER_PAGE } from '../../utils'
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext)
   const [links, setLinks] = useState([])
+  const [cursor, setCursor] = React.useState(null)
   const isNewPage = props.location.pathname.includes('new')
   const isTopPage = props.location.pathname.includes('top')
+  const page = Number(props.match.params.page)
 
   React.useEffect(() => {
     const unsubscribe = getLinks()
     return () => unsubscribe()
-  }, [isTopPage])
+  }, [isTopPage, page])
 
   // defaultのlinksの並び順
   function getLinks() {
+    const hasCursor = Boolean(cursor)
     if (isTopPage) {
       return firebase.db
         .collection('links')
         .orderBy('voteCount', 'desc')
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot)
+      // new pageのfirst page
+    } else if (page === 1) {
+      return firebase.db
+        .collection('links')
+        .orderBy('created', 'desc')
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot)
+    } else if (hasCursor) {
+      return (
+        firebase.db
+          .collection('links')
+          .orderBy('created', 'desc')
+          // afterで含めない、atで含める
+          .startAfter(cursor.created)
+          .limit(LINKS_PER_PAGE)
+          .onSnapshot(handleSnapshot)
+      )
     }
-    return firebase.db
-      .collection('links')
-      .orderBy('created', 'desc')
-      .onSnapshot(handleSnapshot)
   }
 
   function handleSnapshot(snapshot) {
@@ -33,7 +51,23 @@ function LinkList(props) {
       return { id: doc.id, ...doc.data() }
     })
     setLinks(links)
+    const lastLink = links[links.length - 1]
+    setCursor(lastLink)
   }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`)
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`)
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0
 
   return (
     <div>
@@ -42,9 +76,19 @@ function LinkList(props) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   )
 }
